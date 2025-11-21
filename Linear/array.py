2d24fe5d9aafe5baea098ff3b45d613815726cfb
Ankitkumar11414 <kumar06112003@gmail.com>
2025-11-21 22:20:58 +0530

    Updated array

---
+++ Linear/array.py
@@ -0,0 +1,14 @@
+arr = [2,4,6]  ## Blank array
+
+## CREATE
+
+arr.insert(0,5) # insert an element in the beginning of an array -- insert(index,value)
+
+arr.append(8) # insert an element at the last of an array
+
+arr.insert(5,10)
+
+arr1 = [3,7,9,1]
+
+arr = arr + arr1 ## combining two different arrays
+print(arr)
