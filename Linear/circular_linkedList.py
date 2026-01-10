8c7616923ce1600690616743d830b77d5bc19ce7
Ankitkumar11414 <kumar06112003@gmail.com>
2026-01-10 17:13:16 +0530

    Updated Circular LinkedList

--- Linear/circular_linkedList.py
+++ Linear/circular_linkedList.py
@@ -11,7 +11,6 @@ class circular_linkedlist:
 
     def insert_beginning(self,data):
         new_node = Node(data)
-
         ## Empty list
         if self.head == None:
             self.head = new_node
@@ -22,7 +21,7 @@ class circular_linkedlist:
         else:
             temp = self.head
 
-            while temp.next != None:
+            while temp.next != self.head:
                 temp = temp.next
 
             new_node.next = self.head
@@ -51,6 +50,50 @@ class circular_linkedlist:
             print("Inserted sucessfully")
             return
         
+    def insert_position(self, data, pos):
+        new_node = Node(data)
+
+        # Case 1: Empty list
+        if self.head is None:
+            if pos == 0:
+                self.head = new_node
+                new_node.next = new_node
+                print("Node inserted successfully")
+            else:
+                print("Invalid position")
+            return
+
+        # Case 2: Insert at beginning (pos = 0)
+        if pos == 0:
+            temp = self.head
+
+            # Find last node
+            while temp.next != self.head:
+                temp = temp.next
+
+            temp.next = new_node
+            new_node.next = self.head
+            self.head = new_node
+            print("Node inserted successfully")
+            return
+
+        # Case 3: Insert at middle or end
+        curr = self.head
+        index = 0
+
+        while curr.next != self.head and index < pos - 1:
+            curr = curr.next
+            index += 1
+
+        if index != pos - 1:
+            print("Invalid position")
+            return
+
+        new_node.next = curr.next
+        curr.next = new_node
+        print("Node inserted successfully")
+
+        
     def delete_beginning(self):
 
         if self.head == None:
@@ -76,7 +119,7 @@ class circular_linkedlist:
             print("Empty list")
             return
         elif self.head.next == self.head:  ## Single node list
-            self.head == None
+            self.head = None
             print("Deleted sucessfully")
             return
         
@@ -99,13 +142,27 @@ class circular_linkedlist:
         else:
             temp = self.head
             while True:
-                print(temp.data)
+                print(f"{temp.data}")
                 temp = temp.next
                 if temp == self.head:
                     print("List ends")
                     break
     
 
-                
-        
 
+
+l1 = circular_linkedlist()  
+l1.insert_beginning(30)
+l1.insert_beginning(20)
+l1.insert_beginning(10)
+l1.insert_end(40)
+l1.insert_end(70)
+l1.insert_position(50,4)
+l1.insert_position(60,5)
+
+print("------------------------------------------------------------")
+l1.display_linkedlist()
+l1.delete_beginning()
+l1.delete_end()
+
+l1.display_linkedlist()
